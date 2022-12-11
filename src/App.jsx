bd4007b1174dbd3be1eea0b5d9eecb6830322607
lukaslinares <lukaslinares12@gmail.com>
2022-12-10 23:45:21 -0800

    Implementing components with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, Fragment } from 'react'
 import './App.css';
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import Gallery from './Components/Gallery.jsx'
 import SearchBar from './Components/SearchBar.jsx'
 import { DataContext } from './Context/DataContext.jsx'
 import { SearchContext } from './Context/SearchContext.jsx'
+import AlbumView from './Components/AlbumView';
+import ArtistView from './Components/ArtistView';
 
 function App() {
   let [message, setMessage] = useState('Search for Music!');
@@ -30,16 +33,26 @@ function App() {
 
   return (
     <div className="app">
-      <SearchContext.Provider value={{
-        term: searchInput,
-        handleSearch: handleSearch
-      }}>
-        <SearchBar/>
-      </SearchContext.Provider>
       {message}
-      <DataContext.Provider value={data}>
-        <Gallery />
-      </DataContext.Provider>
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <Fragment>
+              <SearchContext.Provider value={{
+                term: searchInput,
+                handleSearch: handleSearch
+              }}>
+                <SearchBar/>
+              </SearchContext.Provider>
+              <DataContext.Provider value={data}>
+                <Gallery />
+              </DataContext.Provider>
+            </Fragment>
+          }/>
+          <Route path='/album/:id' element={<AlbumView/>} />
+          <Route path='/artist/:id' element={<ArtistView/>} />
+       </Routes>
+      </Router>
     </div>
   );
 }
